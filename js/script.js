86926fb112ee0d4625c6031d13baa217110260cf
Tomasz Sypko <tsypko@gmail.com>
2020-07-12 22:37:14 +0200

    code refactored to array & object usage

--- js/script.js
+++ js/script.js
@@ -1,110 +1,74 @@
 {
-  const currencyFromField = document.querySelector(".js-inputCurrencyType");
-  const currencyToField = document.querySelector(".js-outputCurrencyType");
-  const amountField = document.querySelector(".js-form__amount");
+const currencyFromField = document.querySelector(".js-inputCurrencyType");
+const currencyToField = document.querySelector(".js-outputCurrencyType");
+const amountField = document.querySelector(".js-form__amount");
 
-  const pln = 1;
-  const eur = 4.4532;
-  const usd = 3.9395;
-  const gbp = 4.9256;
-  const chf = 4.1702;
+const currencies = [
+  {
+    name: `pln`,
+    rate: 1,
+    flagLink: `images/Flag_of_Poland_2.svg"  alt="Flag of Poland"`
+  },
+  {
+    name: `eur`,
+    rate: 4.4532,
+    flagLink: `images/Flag_of_euro.svg"  alt="Flag of Euro"`
+  },
+  {
+    name: `usd`,
+    rate: 3.9395,
+    flagLink: `images/US_44_Star_Flag.svg"  alt="Flag of US"`
+  },
+  {
+    name: `gbp`,
+    rate: 4.9256,
+    flagLink: `images/Flag_of_the_United_Kingdom.svg"  alt="Flag of GB"`
+  },
+  {
+    name: `chf`,
+    rate: 4.1702,
+    flagLink: `images/Flag_of_Switzerland.svg"  alt="Flag of Swiss"`
+  },
+];
 
-  const exchangeValueIn = (rateIn) => {
-    switch (rateIn) {
-      case "pln":
-        return pln;
-      case "eur":
-        return eur;
-      case "usd":
-        return usd;
-      case "gbp":
-        return gbp;
-      case "chf":
-        return chf;
-    }
-  };
 
-  const exchangeValueOut = (rateOut) => {
-    switch (rateOut) {
-      case "pln":
-        return pln;
-      case "eur":
-        return 1 / eur;
-      case "usd":
-        return 1 / usd;
-      case "gbp":
-        return 1 / gbp;
-      case "chf":
-        return 1 / chf;
-    }
-  };
+const currencyFlagLinkSelect = (flagLink) => {
+  return `<img src="${flagLink} class="form__flag" width="80px" height="50px">`;
+};
 
-  const setCurrency = (currencyName) => {
-    switch (currencyName) {
-      case "pln":
-        return "PLN";
-      case "eur":
-        return "EUR";
-      case "usd":
-        return "USD";
-      case "gbp":
-        return "GBP";
-      case "chf":
-        return "CHF";
-    }
-  };
+const currencyFlagSelect = (fieldIn, fieldOut) => {
+  const currencyFlagFieldIn = document.querySelector(
+    ".js-form__currencyInFlag"
+  );
+  const currencyFlagFieldOut = document.querySelector(
+    ".js-form__currencyOutFlag"
+  );
+  currencyFlagFieldIn.innerHTML = currencyFlagLinkSelect(fieldIn);
+  currencyFlagFieldOut.innerHTML = currencyFlagLinkSelect(fieldOut);
+};
 
-  const currencyFlagImageSelect = (flagLink) => {
-    switch (flagLink) {
-      case "pln":
-        return `images/Flag_of_Poland_2.svg"  alt="Flag of Poland"`;
-      case "eur":
-        return `images/Flag_of_euro.svg"  alt="Flag of Euro"`;
-      case "usd":
-        return `images/US_44_Star_Flag.svg"  alt="Flag of US"`;
-      case "gbp":
-        return `images/Flag_of_the_United_Kingdom.svg"  alt="Flag of GB"`;
-      case "chf":
-        return `images/Flag_of_Switzerland.svg"  alt="Flag of Swiss"`;
-    }
-  };
+const calculateResult = (amount, rateIn, rateOut) =>
+  amount * rateIn * rateOut;
 
-  const currencyFlagLinkSelect = (flagImage) => {
-    const flagLink = currencyFlagImageSelect(flagImage);
-    return `<img src="${flagLink} class="form__flag" width="80px" height="50px">`;
-  };
+const writeResult = () => {
 
-  const currencyFlagSelect = (fieldIn, fieldOut) => {
-    const currencyFlagFieldIn = document.querySelector(
-      ".js-form__currencyInFlag"
-    );
-    const currencyFlagFieldOut = document.querySelector(
-      ".js-form__currencyOutFlag"
-    );
-    currencyFlagFieldIn.innerHTML = currencyFlagLinkSelect(fieldIn);
-    currencyFlagFieldOut.innerHTML = currencyFlagLinkSelect(fieldOut);
-  };
+  const fromCurrencyIndex = currencies.findIndex(({ name }) => name === currencyFromField.value);
+  const toCurrencyIndex = currencies.findIndex(({ name }) => name === currencyToField.value);
 
-  const calculateResult = (amount, rateIn, rateOut) =>
-    amount * rateIn * rateOut;
+  const currencyFrom = currencies[fromCurrencyIndex];
+  const currencyTo = currencies[toCurrencyIndex];
 
-  const setResult = () => {
-    const rateIn = currencyFromField.value;
-    const rateOut = currencyToField.value;
-    const amount = +amountField.value;
-    const currencyFrom = exchangeValueIn(rateIn);
-    const currencyTo = exchangeValueOut(rateOut);
-    currencyFlagSelect(rateIn, rateOut);
-    return calculateResult(amount, currencyFrom, currencyTo);
-  };
+  const amount = +amountField.value;
 
-  const writeResult = () => {
-    const resultCurrency = setCurrency(currencyToField.value);
-    const result = setResult();
-    const resultField = document.querySelector(".js-form__result");
-    resultField.innerText = `${result.toFixed(2)} ${resultCurrency} `;
-  };
-  currencyFromField.addEventListener("input", writeResult);
-  currencyToField.addEventListener("input", writeResult);
-  amountField.addEventListener("input", writeResult);
+  currencyFlagSelect(currencyFrom.flagLink, currencyTo.flagLink);
+
+  const result = calculateResult(amount, currencyFrom.rate, (1 / currencyTo.rate));
+  const resultField = document.querySelector(".js-form__result");
+  resultField.innerText = `${result.toFixed(2)} ${currencyTo.name} `;
+
+};
+
+currencyFromField.addEventListener("input", writeResult);
+currencyToField.addEventListener("input", writeResult);
+amountField.addEventListener("input", writeResult);
 }
